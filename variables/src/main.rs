b17df51f3c058d839fafac724e33dd212947ce5b
Subin Saji <subinssaji@gmail.com>
2024-01-12 11:26:50 +0000

    mut and immutable and const

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,9 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x = 5;
+    println!("The value of x is {x}");
+    x = 6;
+    println!("The value of x is {x}");
+
+    const THREE_HRS_IN_SECS: u32 = 60 * 60 * 30;
+
 }
-
-
-
-
-
-test
